396857abd298a328a589c1f5030f77c473de4d3e
typAhmedSleem <theyoungprogrammer67@gmail.com>
2023-11-06 15:12:59 +0200

    refactor: moved some stuff from functions.py to constants.py

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,10 @@ DEF_QLT_FACTOR = 80
 RESIZE_SHAPE = (600, 720)
 RESIZE = False
 
+
 # IO
-BASE_PATH = "C:\\Users\\typ\\Development\\algorithms\\JPEG\\src\\images"
+LOGGER_ENABLED: Final = True
 FILENAME = "original (8)"
+BASE_PATH = "C:\\Users\\typ\\Development\\algorithms\\JPEG\\src\\images"
 INPUT_PATH: Final = f"{BASE_PATH}\\input\\{FILENAME}.jpeg"
 OUTPUT_PATH: Final = f"{BASE_PATH}\\output\\{FILENAME}.{FILE_EXT}"
